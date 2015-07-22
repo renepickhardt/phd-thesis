b4a552a661e8ec40a70138014cc405e8ae3b2078
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2015-07-22 17:45:26 +0200

    SRILM installs fully automatically

---
+++ toolkit-benchmark/install/wizard/srilm-installer.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+#
+# Note: This script is not intended to be executed directly.
+# It is part of the installation wizard of ULMA.
+# Please don't run this script directly, but run the installation wizard instead.
+#
+# ULMA (unified language model API) supports various tools and toolkits and provides one single API to use any of them.
+#
+# Authors:
+# Sebastian Schlicht (sebastian@jablab.de)
+#
+
+# Installs SRILM and its dependencies
+#
+# # SRILM 1.7.1
+# http://www.speech.sri.com/projects/srilm/
+#
+# ## Installation target
+# './srilm-1.7.1/' which is stored in `$SRILM`
+# Binaries will be found in './bin/<architecture>/'
+#
+# ## Dependencies
+# * g++ 3.4.3+ (via apt)
+# * make (via apt)
+# 
+function install_srilm {
+  # license agreement
+  if ! acceptLicense 'SRILM' 'http://www.speech.sri.com/projects/srilm/docs/License'; then
+    return 1
+  fi
+  
+  # download
+  SRILM_DIR='srilm-1.7.1'
+  SRILM_FILE=$SRILM_DIR'.tar.gz'
+  if [ ! -f $SRILM_FILE ]; then
+    # collect user data for download form
+    echo 'In order to use SRILM SRI requests you to provide some information.'
+    echo 'The wizard will forward the information to SRI and download SRILM for you.'
+    echo
+    ## name
+    read -p 'Name: ' NAME
+    ## organization
+    read -p 'Organization: ' ORGANIZATION
+    ## address
+    read -p 'Address: ' ADDRESS
+    ## email
+    read -p 'eMail-Address: ' EMAIL
+    ## website (optional)
+    read -p 'Website (optional): ' WEBSITE
+    ## receive updates per mail
+    while true; do
+      read -p 'Do you want to retrieve updates per eMail? ' yn
+      case $yn in
+        [Yy]*) RECUP=true;;
+        [Nn]*) RECUP=false;;
+        *)
+          echo 'Invalid choice. Please answer with yes or no.'
+          continue
+          ;;
+      esac
+      break
+    done
+  
+    echo 'Please wait while the wizard downloads your copy of SRILM.'
+    wget --post-data="$POST_DATA" 'http://www.speech.sri.com/projects/srilm/srilm_download.php' -O $SRILM_FILE
+    SUCCESS=$?
+    # TODO: -sS
+    #curl -X POST -o srilm-1.7.1.tar.gz -F 'WWW_file=srilm-1.7.1.tar.gz' -F "WWW_name=$NAME" -F "WWW_org=$ORGANIZATION" -F "WWW_address=$ADDRESS" -F "WWW_email=$EMAIL" -F "WWW_url=$WEBSITE" -F "WWW_list=$RECUP" 'http://www.speech.sri.com/projects/srilm/srilm_download.php'
+    
+    if [ "$SUCCESS" -ne 0 ]; then
+      echo 'Failed to download SRILM!'
+      return 2
+    fi
+    echo 'Download completed.'
+  fi
+  # verify download
+  SRILM_FILE_SIZE=$(wc -c <"$SRILM_FILE")
+  if [ ! -f "$SRILM_FILE" -o "$SRILM_FILE_SIZE" -ne "65498380" ]; then
+    echo 'The installation file is corrupt.'
+    echo 'Please download SRILM manually from http://www.speech.sri.com/projects/srilm/download.html'
+    return 3
+  fi
+  
+  # install dependencies
+  requirePackage g++
+  requirePackage make
+  
+  # extract
+  if [ ! -d $SRILM_DIR ]; then
+    # SRILM is a flat archive, we have to create the target directory on our own
+    mkdir $SRILM_DIR
+    cd $SRILM_DIR
+    tar -xf ../$SRILM_FILE
+    if [ $? -ne 0 ]; then
+      echo 'Failed to extract SRILM!'
+      return 4
+    fi
+    echo 'SRILM was extracted successfully.'
+    cd ..
+  fi
+  
+  # install
+  ## prepare
+  ### set SRILM variable in Makefile (point at SRILM directory)
+  DIR=$( getCurrentDir )
+  MAKEFILE_SRILM=$( echo "SRILM = ${DIR}/${SRILM_DIR}" | escapeSlashes )
+  cd ${SRILM_DIR}
+  sed -i "s/^# SRILM =.*/${MAKEFILE_SRILM}/" Makefile
+  cd ..
+  ## compile
+  cd ${SRILM_DIR}
+  make && make cleanest
+  SUCCESS=$?
+  cd ..
+  
+  if [ $SUCCESS ]; then
+    # export installation directory to environment variable
+    #setEnvVar 'SRILM="'"$SRILM_DIR"'"'
+    echo 'SRILM was installed successfully.'
+  fi
+  return $SUCCESS
+}
+
---
+++ toolkit-benchmark/install/wizard/ulma-installer.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# ULMA installer
+# Installation wizard for language model toolkits supported by ULMA.
+#
+
+# Returns the absolute path to the current directory.
+function getCurrentDir {
+  echo $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+}
+
+# Escapes slashes for the usage in sed.
+function escapeSlashes {
+  sed 's/\//\\\//g'
+}
+
+# Checks if a package is installed.
+function checkPackage {
+  PCKG_INSTALLED=$( dpkg-query -W --showformat='${Status}\n' $1|grep "install ok installed" 2>/dev/null )
+  if [ -n "${PCKG_INSTALLED}" ]; then
+    return 0
+  fi
+  return 1
+}
+
+# Installs a package if it's missing.
+function requirePackage {
+  if ! checkPackage "$1"; then
+    echo 'Package '"'$1'"' is missing and will be installed now.'
+    sudo apt-get install "$1"
+    SUCCESS="$?"
+    if [ "$SUCCESS" -ne 0 ]; then
+      echo 'Failed to install package '"'$1'! Please install the package manually and try again."
+    fi
+    return $SUCCESS
+  fi
+  return 0
+}
+
+# Sets an environment variable permanently.
+function setEnvVar {
+  echo 'export '"$1" >> ~/.profile
+}
+
+# Installs Boost 1.58 locally.
+function installBoost {
+  # TODO
+  return 0
+}
+
+# Asks the user to accept the license of a toolkit.
+# The choice of the user will be returned as a boolean value.
+function acceptLicense {
+  cmd=(dialog --title "$1"' license agreement' --no-tags --radiolist 'By selecting '"'"'Yes'"'"' you accept the license of '"$1"'.
+You can find a copy of this license at '"$2"'
+
+If you reject the license, the installation will be skipped.' 22 76 16)
+  options=(
+    0 Yes off
+    1 No off
+  )
+  choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+  clear
+  return $choice
+}
+
+# we need dialog for the GUI
+if ! requirePackage dialog; then
+  exit 9
+fi
+
+cmd=(dialog --title 'ULMA installation wi#zard' --separate-output --checklist "Select the toolkits you want to install:" 22 76 16)
+options=(
+  1 "SRILM" on
+)
+choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+clear
+
+# collect license aggreement
+for choice in $choices
+do
+  case $choice in
+    1)
+      source srilm-installer.sh
+      install_srilm
+      ;;
+    2)
+      # TODO
+      exit 1
+      ;;
+  esac
+done
+
