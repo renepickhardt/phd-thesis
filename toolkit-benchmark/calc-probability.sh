61d82bd80a61787fb68961499ad5e278fb6600ab
Sebastian Schlicht <sebschlicht@uni-koblenz.de>
2015-08-22 18:09:32 +0200

    query file creation added

--- toolkit-benchmark/calc-probability.sh
+++ toolkit-benchmark/calc-probability.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 TRAINING_FILE=training.txt
 VOCAB_FILE=training.txt.vocab
-ORDER=1
+ORDER="$1"
+
+if [ -z "$ORDER" ] || [ "$ORDER" -lt "1" ]; then
+  echo 'Please pass an order (higher than zero)!'
+  exit 1
+fi
 
 # use SRILM to create vocabulary
 function create_vocab {
   srilm-1.7.1/bin/*/ngram-count -order "$ORDER" -write-vocab "$VOCAB_FILE" -text "$TRAINING_FILE"
 }
-create_vocab
+#create_vocab
 
 # create all the language models
 function create_models {
@@ -34,16 +39,49 @@ function prepare_for_queries {
 #prepare_for_queries
 
 # create query files
-function create_query_file_srilm {
-  awk '{print $0}' "$VOCAB_FILE" > /tmp/srilm-query
-  awk '{print $0" 1"}' /tmp/srilm-query > "$1"
+function expand_query_file {
+  # combine each line of vocabulary with each query file line
+  TMP_FILE="$1"
+  QUERY_FILE="$2"
+  
+  if [ -a "$TMP_FILE" ]; then
+    >"$TMP_FILE"
+  else
+    touch "$TMP_FILE"
+  fi
+  
+  if [ ! -s "$QUERY_FILE" ]; then
+    echo 'query file is empty, using vocabulary copy.'
+    cp "$VOCAB_FILE" "$TMP_FILE"
+  else
+    echo 'expand query file by vocabulary...'
+    while read line; do
+      awk '{print $0" '"$line"'"}' "$QUERY_FILE" >> "$TMP_FILE"
+    done <"$VOCAB_FILE"
+  fi
+  mv "$TMP_FILE" "$QUERY_FILE"
 }
-function create_query_file_kenlm {
-  awk '{print $0}' "$VOCAB_FILE" > /tmp/kenlm-query
+function create_query_files {
+  QUERY_FILE=/tmp/kenlm-query
+  TMP_FILE=/tmp/ulma-query.tmp
+  
+  if [ -a "$QUERY_FILE" ]; then
+    >"$QUERY_FILE"
+  fi
+  
+  i=0
+  while [ "$i" -lt "$ORDER" ]; do
+    expand_query_file "$TMP_FILE" "$QUERY_FILE"
+    let i=i+1
+  done
+
+  echo 'finishing SRILM query file...'
+  awk '{print $0" 1"}' "$QUERY_FILE" > "$QUERY_SRILM"
+  mv "$QUERY_FILE" "$QUERY_KENLM"
 }
 QUERY_SRILM=overview/query/srilm-"$ORDER".txt
-
-create_query_file_srilm "$QUERY_SRILM"
+QUERY_KENLM=overview/query/kenlm-"$ORDER".txt
+create_query_files
 
 # query the probabilites of the vocabulary entries
 
